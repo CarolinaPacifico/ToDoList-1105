df25186e20d0ffe2f788cdaff3ca2b01091e479b
CarolinaPacifico <84384263+CarolinaPacifico@users.noreply.github.com>
2022-05-26 08:49:47 -0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -27,6 +27,6 @@ module.exports = (app)=>{
         if(!comparar){
             return res.send("Senha Inválida!")
         }
-        res.render("atividades.ejs")
+        res.render("atividades.ejs",{nome:verificar.nome, id:verificar._id})
     })
 }
